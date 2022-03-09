2552709e2701c63c013dc8e44f8061978ceb54c1
tony <chcjswo@gmail.com>
2022-03-09 23:31:41 +0900

    이메일 로그인 메일 보내기 적용

--- src/main/java/com/mocadev/studyolle/account/AccountController.java
+++ src/main/java/com/mocadev/studyolle/account/AccountController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
@@ -83,11 +84,49 @@ public class AccountController {
 							  @CurrentUser Account account) {
 		final Account byNickname = accountRepository.findByNickname(nickname);
 		if (nickname == null) {
-			throw  new IllegalArgumentException(nickname + "에 해당하는 유저가 없습니다.");
+			throw new IllegalArgumentException(nickname + "에 해당하는 유저가 없습니다.");
 		}
 
 		model.addAttribute(byNickname);
 		model.addAttribute("isOwner", byNickname.equals(account));
 		return "account/profile";
+	}
+
+	@GetMapping("/email-login")
+	public String emailLoginForm() {
+		return "account/email-login";
+	}
+
+	@PostMapping("/email-login")
+	public String sendEmailLoginLink(String email,
+									 Model model,
+									 RedirectAttributes attributes) {
+		Account account = accountRepository.findByEmail(email);
+		if (account == null) {
+			model.addAttribute("error", "유효한 이메일 주소가 아닙니다.");
+			return "account/email-login";
+		}
+
+		if (!account.canSendConfirmEmail()) {
+			model.addAttribute("error", "이메일 로그인은 1시간 뒤에 사용할 수 있습니다.");
+			return "account/email-login";
+		}
+
+		accountService.sendLoginLink(account);
+		attributes.addFlashAttribute("message", "이메일 인증 메일을 발송했습니다.");
+		return "redirect:/email-login";
+	}
+
+	@GetMapping("/login-by-email")
+	public String loginByEmail(String token, String email, Model model) {
+		Account account = accountRepository.findByEmail(email);
+		String view = "account/logged-in-by-email";
+		if (account == null || !account.isValidToken(token)) {
+			model.addAttribute("error", "로그인할 수 없습니다.");
+			return view;
+		}
+
+		accountService.login(account);
+		return view;
 	}
 }
